cee5bc8509f09572c340e093a8591124a7f56448
Samuel Silva Costa <sscosta2003@outlook.com>
2021-08-26 22:09:48 -0300

    Segundo Commit

--- primeiro.php
+++ primeiro.php
@@ -1,3 +1,5 @@
+<!-- Primeiro PHP !-->
+
 <?php 
 
 	echo "Teste";
@@ -5,3 +7,76 @@
 	echo "<br> Teste 2";
 
  ?>
+
+<!-- Segunda PHP !-->
+
+<?php 
+	$data = "28 de agosto de 2020";
+	$salario = 850.00;
+	$cargo = "Estagiário";
+	$idade = 18;
+	$resultado = true;
+
+	echo "<br> <br> Arquivo iniciado em $data <br> ";
+	printf("Salário atual: %.2f <br>", $salario);
+	printf("Cargo: %s <br>", $cargo);
+	$texto = sprintf("Nosso ".$cargo." recebe ".$salario." por mês e têm ".$idade." anos");
+	echo $texto;
+
+	if($resultado)
+	{
+		echo "<br> Validade: Verdadeiro!";
+	}
+
+ ?>
+
+ <!-- Terceiro PHP !-->
+
+ <?php 
+
+ $fundo = "azul";
+ $FUNDO = "Vermelho";
+ $Fundo = "Rosa";
+ 
+ echo "<br> <br> João tem uma casa com o fundo ".$fundo." Já Maria tem a casa com o fundo ".$Fundo." e por ultimo Carlos tem a casa com fundo ".$FUNDO;
+ ?>
+
+ <!-- Quarto PHP !--> 
+
+<?php
+
+$ano = 2020;
+
+ function Exibir($parametro)
+ {
+ 	$parametro += 4;
+ 	return $parametro;
+ }
+
+ echo " <br> <br> Estamos em 2020, daqui a 4 anos o mundo vai acabar mas, será ".Exibir($ano);
+
+
+ ?>
+
+ <!-- Quinto PHP !--> 
+
+<?PHP
+
+function Incremento()
+{
+	STATIC $valor = 2020;
+	$valor++;
+	return $valor;
+}
+
+echo "<br> ".Incremento();
+echo "<br> ".Incremento();
+echo "<br> ".Incremento();
+echo "<br> ".Incremento();
+
+
+?>
+
+<!-- Sexto PHP !--> 
+
+
